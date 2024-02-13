3595f206314c09a8e8e2bf17737849d3673e50ef
Nick Putkaradze <nick.putkaradze@opendaq.com>
2024-02-13 08:50:58 +0400

    Rework tests

--- tests/jet_server_test.cpp
+++ tests/jet_server_test.cpp
@@ -4,262 +4,173 @@
 // Checks whether all of the required Jet states are present
 TEST_F(JetServerTest, CheckStatePresence)
 {
-    myJet->publishJetStates();
+    // Publish device structure as Jet states
+    jetServer->publishJetStates();
     
     // Read Jet states
-    readJetStates();
+    Json::Value jetStates = readJetStates();
+    // Get Jet state paths
+    std::vector<std::string> jetStatePaths = getJetStatePaths(jetStates);
+    // Get component IDs of openDAQ objects
+    std::vector<std::string> globalIds = getComponentIDs(rootDevice);
 
-    // Check whether all of the Jet states are present
-    ASSERT_EQ(jetStateExists(rootDeviceJetPath), true);
-    parseFolder(rootDevice, jetStatePath);
+    // Check whether there same amount of states in Jet as there are high level objects in openDAQ
+    ASSERT_EQ(jetStatePaths.size(), globalIds.size());
+
+    // Sort both vectors to ensure elements are in a consistent order
+    std::sort(jetStatePaths.begin(), jetStatePaths.end());
+    std::sort(globalIds.begin(), globalIds.end());
+    // Check whether individual Jet state paths match global IDs of openDAQ objects
+    bool statesMatch = std::equal(jetStatePaths.begin(), jetStatePaths.end(), globalIds.begin());
+    ASSERT_TRUE(statesMatch);
 }
 
 // Ensures functionality of Boolean property
 TEST_F(JetServerTest, TestBoolProperty)
 {
+    bool valueInJet;
+    bool valueInOpendaq;
+
     // Add bool property to the device
     std::string propertyName = "TestBool";
     rootDevice.addProperty(BoolProperty(propertyName, true));
 
-    myJet->publishJetStates();
+    jetServer->publishJetStates();
 
     // Check whether values are equal initially
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            bool valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asBool();
-            bool valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            ASSERT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
+    valueInJet = getPropertyValueInJet(propertyName, false).asBool();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    ASSERT_EQ(valueInJet, valueInOpendaq);
+ 
     // Check whether property value updated from openDAQ updates value in Jet
     rootDevice.setPropertyValue(propertyName, false);
-    readJetStates(); // We have to update publishedJsonValue as we have changed a property value
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            bool valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asBool();
-            bool valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    valueInJet = getPropertyValueInJet(propertyName, true).asBool();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 
     // Check whether property value updated from Jet updates value in openDAQ
-    Json::Value root;
-    root[propertyName] = true;
-    Json::StreamWriterBuilder builder;
-    const std::string jsonValue = Json::writeString(builder, root);
-    modifyJetState("json", rootDeviceJetPath, jsonValue.c_str());
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            bool valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asBool();
-            bool valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    setPropertyValueInJet(propertyName, true);
+    valueInJet = getPropertyValueInJet(propertyName, false).asBool();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 }
 
 // Ensures functionality of Integer property
 TEST_F(JetServerTest, TestIntProperty)
 {
-    // Add bool property to the device
+    int64_t valueInJet;
+    int64_t valueInOpendaq;
+
+    // Add integer property to the device
     std::string propertyName = "TestInt";
     rootDevice.addProperty(IntProperty(propertyName, 69420));
 
-    myJet->publishJetStates();
+    jetServer->publishJetStates();
 
     // Check whether values are equal initially
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            int64_t valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asInt64();
-            int64_t valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            ASSERT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
+    valueInJet = getPropertyValueInJet(propertyName, -1).asInt64();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    ASSERT_EQ(valueInJet, valueInOpendaq);
+ 
     // Check whether property value updated from openDAQ updates value in Jet
     rootDevice.setPropertyValue(propertyName, 424242);
-    readJetStates(); // We have to update publishedJsonValue as we have changed a property value
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            int64_t valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asInt64();
-            int64_t valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    valueInJet = getPropertyValueInJet(propertyName, -1).asInt64();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 
     // Check whether property value updated from Jet updates value in openDAQ
-    Json::Value root;
-    root[propertyName] = 999666;
-    Json::StreamWriterBuilder builder;
-    const std::string jsonValue = Json::writeString(builder, root);
-    modifyJetState("json", rootDeviceJetPath, jsonValue.c_str());
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            int64_t valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asInt64();
-            int64_t valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-}
-
-// Ensures functionality of String property
-TEST_F(JetServerTest, TestStringProperty)
-{
-    // Add bool property to the device
-    std::string propertyName = "TestString";
-    rootDevice.addProperty(StringProperty(propertyName, "Richard Feynman"));
-
-    myJet->publishJetStates();
-
-    // Check whether values are equal initially
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            std::string valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asString();
-            std::string valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            ASSERT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
-    // Check whether property value updated from openDAQ updates value in Jet
-    rootDevice.setPropertyValue(propertyName, "Richard Dawkins");
-    readJetStates(); // We have to update publishedJsonValue as we have changed a property value
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            std::string valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asString();
-            std::string valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
-    // Check whether property value updated from Jet updates value in openDAQ
-    Json::Value root;
-    root[propertyName] = "John Cena";
-    Json::StreamWriterBuilder builder;
-    const std::string jsonValue = Json::writeString(builder, root);
-    modifyJetState("json", rootDeviceJetPath, jsonValue.c_str());
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            std::string valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asString();
-            std::string valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    setPropertyValueInJet(propertyName, 999666);
+    valueInJet = getPropertyValueInJet(propertyName, -1).asInt64();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 }
 
 // Ensures functionality of Float property
 TEST_F(JetServerTest, TestFloatProperty)
 {
-    // Add bool property to the device
+    double valueInJet;
+    double valueInOpendaq;
+
+    // Add float property to the device
     std::string propertyName = "TestFloat";
     rootDevice.addProperty(FloatProperty(propertyName, 69420.69));
 
-    myJet->publishJetStates();
+    jetServer->publishJetStates();
 
     // Check whether values are equal initially
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            double valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asDouble(); // in openDAQ ctFloat CoreType is actually double
-            double valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            ASSERT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
+    valueInJet = getPropertyValueInJet(propertyName, -1).asDouble();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    ASSERT_EQ(valueInJet, valueInOpendaq);
+ 
     // Check whether property value updated from openDAQ updates value in Jet
     rootDevice.setPropertyValue(propertyName, 424242.12413);
-    readJetStates(); // We have to update publishedJsonValue as we have changed a property value
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            double valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asDouble();
-            double valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    valueInJet = getPropertyValueInJet(propertyName, -1).asDouble();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 
     // Check whether property value updated from Jet updates value in openDAQ
-    Json::Value root;
-    root[propertyName] = 999666.77713;
-    Json::StreamWriterBuilder builder;
-    const std::string jsonValue = Json::writeString(builder, root);
-    modifyJetState("json", rootDeviceJetPath, jsonValue.c_str());
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            double valueInJet = item[hbk::jet::VALUE].get(propertyName, "").asDouble();
-            double valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    setPropertyValueInJet(propertyName, 999666.77713);
+    valueInJet = getPropertyValueInJet(propertyName, -1).asDouble();
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
+}
+
+// Ensures functionality of String property
+TEST_F(JetServerTest, TestStringProperty)
+{
+    std::string valueInJet;
+    // std::string valueInOpendaq;
+
+    // Add string property to the device
+    std::string propertyName = "TestString";
+    rootDevice.addProperty(StringProperty(propertyName, "Richard Feynman"));
+
+    jetServer->publishJetStates();
+
+    // Check whether values are equal initially
+    valueInJet = getPropertyValueInJet(propertyName, "").asString();
+    std::string valueInOpendaq1 = rootDevice.getPropertyValue(propertyName);
+    ASSERT_EQ(valueInJet, valueInOpendaq1);
+ 
+    // Check whether property value updated from openDAQ updates value in Jet
+    rootDevice.setPropertyValue(propertyName, "Richard Dawkins");
+    valueInJet = getPropertyValueInJet(propertyName, "").asString();
+    std::string valueInOpendaq2 = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq2);
+
+    // Check whether property value updated from Jet updates value in openDAQ
+    setPropertyValueInJet(propertyName, "John Cena");
+    valueInJet = getPropertyValueInJet(propertyName, "").asString();
+    std::string valueInOpendaq3 = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq3);
 }
 
 // Ensures functionality of List property
 TEST_F(JetServerTest, TestListProperty)
 {
-    // Add bool property to the device
+    ListPtr<BaseObjectPtr> valueInJet;
+    ListPtr<BaseObjectPtr> valueInOpendaq;
+
+    // Add list property to the device
     std::string propertyName = "TestList";
     rootDevice.addProperty(ListProperty(propertyName, List<std::string>("Georgia", "is", "beautiful")));
 
-    myJet->publishJetStates();
+    jetServer->publishJetStates();
 
     // Check whether values are equal initially
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            ListPtr<BaseObjectPtr> valueInJet = myJet->convertJsonArrayToDaqArray(rootDevice, propertyName, item[hbk::jet::VALUE]);
-            ListPtr<BaseObjectPtr> valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            ASSERT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
-
+    valueInJet = jetServer->convertJsonArrayToDaqArray(rootDevice, propertyName, getPropertyListInJet(propertyName));
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    ASSERT_EQ(valueInJet, valueInOpendaq);
+ 
     // Check whether property value updated from openDAQ updates value in Jet
     rootDevice.setPropertyValue(propertyName, List<std::string>("Optimus", "Prime"));
-    readJetStates(); // We have to update publishedJsonValue as we have changed a property value
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            ListPtr<BaseObjectPtr> valueInJet = myJet->convertJsonArrayToDaqArray(rootDevice, propertyName, item[hbk::jet::VALUE]);
-            ListPtr<BaseObjectPtr> valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    valueInJet = jetServer->convertJsonArrayToDaqArray(rootDevice, propertyName, getPropertyListInJet(propertyName));
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 
     // Check whether property value updated from Jet updates value in openDAQ
-    Json::Value root;
-    Json::Value array(Json::arrayValue);
-    std::vector<std::string> values = {"The", "first", "principle", "is", "that", "you", "must", "not", "fool", "yourself", "and", "you", "are", "the", "easiest", "person", "to", "fool."};
-    for (auto val : values) {
-        array.append(val);
-    }
-    root[propertyName] = array;
-    Json::StreamWriterBuilder builder;
-    const std::string jsonValue = Json::writeString(builder, root);
-    modifyJetState("json", rootDeviceJetPath, jsonValue.c_str());
-    readJetStates();
-    for (const Json::Value& item : publishedJsonValue) {
-        if(item[hbk::jet::PATH] == rootDeviceJetPath)
-        {
-            ListPtr<BaseObjectPtr> valueInJet = myJet->convertJsonArrayToDaqArray(rootDevice, propertyName, item[hbk::jet::VALUE]);
-            ListPtr<BaseObjectPtr> valueInOpendaq = rootDevice.getPropertyValue(propertyName);
-            EXPECT_EQ(valueInJet, valueInOpendaq);
-        }
-	}
+    setPropertyListInJet(propertyName, {"The", "first", "principle", "is", "that", "you", "must", "not", "fool", "yourself", "and", "you", "are", "the", "easiest", "person", "to", "fool."});
+    valueInJet = jetServer->convertJsonArrayToDaqArray(rootDevice, propertyName, getPropertyListInJet(propertyName));
+    valueInOpendaq = rootDevice.getPropertyValue(propertyName);
+    EXPECT_EQ(valueInJet, valueInOpendaq);
 }
