6167b45851a6bfabd275c43481d800b8ac7159e5
Nick Putkaradze <nick.putkaradze@opendaq.com>
2024-02-26 10:26:05 +0400

    rework event handling

--- include/jet_server.h
+++ include/jet_server.h
@@ -32,22 +32,32 @@ public:
     void publishJetStates();
 
 private:
-    // Tree structure parsers of an openDAQ device
+    // Tree structure parsers and Jet state preparators
     void parseFolder(const FolderPtr& parentFolder);
     void prepareComponentJetState(const ComponentPtr& component);
-    void parseComponentProperties(const ComponentPtr& component);
+    void parseComponentProperties(const ComponentPtr& component, Json::Value& parentJsonValue);
+    template <typename ObjectType>
+    void parseComponentSignals(const ObjectType& object, Json::Value& parentJsonValue);
+    void prepareSignalJetState(const SignalPtr& signal);
+    void parseComponentInputPorts(const FunctionBlockPtr& functionBlock, Json::Value& parentJsonValue);
+    void prepareInputPortJetState(const InputPortPtr& inputPort);
 
-    // Tree structure publisher of an openDAQ device
-    void publishComponentJetState(const std::string& path);
+    // Defines callback function for an openDAQ component
+    void createComponentCallback(const ComponentPtr& component);
 
-    // Functions which are called when some component's property is updated from Jet (e.g. using jetset tool)
-    void updateJetState(const PropertyObjectPtr& propertyObject);
-    void updateJetState(const ComponentPtr& component);
+    // Publishes an openDAQ component as a Jet state
+    void publishComponentJetState(const std::string& path, const Json::Value& jetState);
 
     // Helper function which determines type of an openDAQ property
     template <typename PropertyHolder>
     void determinePropertyType(const PropertyHolder& propertyHolder, const PropertyPtr& property, Json::Value& parentJsonValue);
 
+    // Creates a callable Jet method, which diverts the call to corresponding openDAQ function or procedure
+    void createJetMethod(const ComponentPtr& propertyPublisher, const PropertyPtr& property);
+
+    // ********************************************************************************************************************************************
+    //                          Functions which append different types of information of openDAQ objects to Jet states
+    // ********************************************************************************************************************************************
     // Append properties to Json value
     template<typename PropertyHolderType, typename DataType>
     void appendSimpleProperty(const PropertyHolderType& propertyHolder, const std::string& propertyName, Json::Value& parentJsonValue);
@@ -64,23 +74,10 @@ private:
     template<typename PropertyHolderType>
     void appendStructProperty(const PropertyHolderType& propertyHolder, const PropertyPtr& property, Json::Value& parentJsonValue);
 
-    // List and Dict property helper functions
-    template <typename PropertyHolderType, typename ItemType>
-    void fillListProperty(const PropertyHolderType& propertyHolder, const std::string& propertyName, Json::Value& parentJsonValue);
-    template <typename PropertyHolderType>
-    void fillListPropertyWithRatio(const PropertyHolderType& propertyHolder, const std::string& propertyName, Json::Value& parentJsonValue);
-    template <typename PropertyHolderType, typename KeyType>
-    void determineDictItemType(const PropertyHolderType& propertyHolder, const PropertyPtr& property, Json::Value& parentJsonValue);
-    template <typename PropertyHolderType, typename KeyType, typename ItemType>
-    void fillDictProperty(const PropertyHolderType& propertyHolder, const PropertyPtr& property, Json::Value& parentJsonValue);
-
     // Append other objects to Json value
     void appendDeviceMetadata(const DevicePtr& device, Json::Value& parentJsonValue);
     void appendDeviceDomain(const DevicePtr& device, Json::Value& parentJsonValue);
     void appendFunctionBlockInfo(const FunctionBlockPtr& functionBlock, Json::Value& parentJsonValue);
-    void appendInputPorts(const FunctionBlockPtr& functionBlock, Json::Value& parentJsonValue);
-    template <typename ObjectType>
-    void appendOutputSignals(const ObjectType& object, Json::Value& parentJsonValue);
 
     // Append common metadata to Json value
     void appendGlobalId(const ComponentPtr& component, Json::Value& parentJsonValue);
@@ -89,18 +86,33 @@ private:
     void appendVisibleStatus(const ComponentPtr& component, Json::Value& parentJsonValue);
     void appendTags(const ComponentPtr& component, Json::Value& parentJsonValue);
 
-    bool propertyCallbacksCreated;
-    void createCallbackForProperty(const PropertyPtr& property);
-    
-    void createJetMethod(const ComponentPtr& propertyPublisher, const PropertyPtr& property);
+
+    // ********************************************************************************************************************************************
+    //                                          Update functions addressing change events from openDAQ
+    //!    These functions are also called when change is requested from Jet. This happens in order to update appropriate Jet state as well
+    // ********************************************************************************************************************************************
+
+    // Update Jet state attributes
+    void OPENDAQ_EVENT_updateProperty(const ComponentPtr& component, const DictPtr<IString, IBaseObject>& eventParameters);
+    template <typename DataType>
+    void OPENDAQ_EVENT_updateSimpleProperty(const ComponentPtr& component, const DictPtr<IString, IBaseObject>& eventParameters);
+    void OPENDAQ_EVENT_updateListProperty(const ComponentPtr& component, const DictPtr<IString, IBaseObject>& eventParameters);
+    void OPENDAQ_EVENT_updateDictProperty(const ComponentPtr& component, const DictPtr<IString, IBaseObject>& eventParameters);
+    void OPENDAQ_EVENT_updateActiveStatus(const ComponentPtr& component, const DictPtr<IString, IBaseObject>& eventParameters);
+
+    // ********************************************************************************************************************************************
+    //                              Update functions addressing change events from Jet (e.g. using "jetset" tool)
+    // ********************************************************************************************************************************************
+    void JET_EVENT_updateProperty(const ComponentPtr& component, const std::string& propertyName, const Json::Value& newPropertyValue);
+    template <typename DataType>
+    void JET_EVENT_updateSimpleProperty(const ComponentPtr& component, const std::string& propertyName, const DataType& newPropertyValue);
+    void JET_EVENT_updateListProperty(const ComponentPtr& component, const std::string& propertyName, const Json::Value& newJsonArray);
+    void JET_EVENT_updateDictProperty(const ComponentPtr& component, const std::string& propertyName, const Json::Value& newJsonDict);
+    void JET_EVENT_updateActiveStatus(const ComponentPtr& component, const Json::Value& newActiveStatus);
+
 
     DevicePtr rootDevice; // Pointer to the root openDAQ device whose tree structure is parsed in order to publish it as Jet states
-    DictPtr<IString, IComponent> componentIdDict; // Stores global IDs of components and their pointers
-    Json::Value jsonValue; // Container which is filled for every component and is cleared when the component's tree structure is published as a Jet state
-
     hbk::jet::PeerAsync* jetPeer;
-
-    bool jetStateUpdateDisabled;
 
     hbk::sys::EventLoop jetEventloop;
     bool jetEventloopRunning;
